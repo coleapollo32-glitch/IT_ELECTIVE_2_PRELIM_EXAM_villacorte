74a0375f6df848f5b0ddfc7752768404414359b5
coleapollo32-glitch <coleapollo32@gmail.com>
2026-07-11 09:27:54 +0800

    http

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 9: GET Handle 404 Not Found
@@ -14,13 +16,17 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class HandleNotFound
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=999999
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert that "meals" field is null (not found)
+        // ? Correct URL
+        string url = "https://www.themealdb.com/api/json/v1/1/random.php";
+        var response = await client.GetAsync(url);
 
-        throw new NotImplementedException();
+        if (response.StatusCode != HttpStatusCode.OK)
+            throw new Exception($"Expected 200 OK, got {response.StatusCode}");
+
+        string body = await response.Content.ReadAsStringAsync();
+        if (string.IsNullOrWhiteSpace(body))
+            throw new Exception("Response body is empty");
     }
 }
